049548f5ee9785ae00dc009f49d4f61ad07cd0f3
JorBail <chewydog@uw.edu>
2023-02-20 23:13:22 -0800

    Spelling Error and Axis Titles

--- Chart2.R
+++ Chart2.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(ggplot2)
 library(plotly)
 tinder <-read.csv("Tinder Millennial Match Rate Dataset.csv", stringsAsFactors = FALSE)
+tinder <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-alexis-dimas/main/Tinder%20Millennial%20Match%20Rate%20Dataset.csv", stringsAsFactors = FALSE)
 tinderydf<- tinder %>%
   group_by(Answer) %>%
   filter(It.became.a.Relationship == 'Yes') %>% 
@@ -16,12 +17,14 @@ tinderndf<- tinder %>%
   summarize(count = sum(choice_counts)) %>% 
   mutate(outcome = 'No')
 
+
 tinder_groups <- c(tinderydf$Answer, tinderndf$Answer)
-outcome <- c(tinderydf$outcome, tinderndf$outcome)
-count <- c(tinderydf$count, tinderndf$count)
+Relationship <- c(tinderydf$outcome, tinderndf$outcome)
+Users <- c(tinderydf$count, tinderndf$count)
 tinderdf <- data.frame(tinder_groups, outcome, count)
 
-ggplot(tinderdf, aes(x = outcome, y = count, fill = tinder_groups)) + geom_bar( stat = 'identity') + labs(title = 'Relationship sucess based on usage of Tinder')
-
-
-
+ggplot(
+  tinderdf, 
+  aes(x = Relationship, y = Users, fill = tinder_groups)) + 
+  geom_bar( stat = 'identity') + 
+  labs(title = 'Relationship success based on usage of Tinder')
