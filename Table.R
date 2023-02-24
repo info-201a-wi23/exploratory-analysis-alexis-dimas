cd89ae6ace1b0e91728d7b9c5695c4e72d666535
Mercedes Ro <mercedesro@Mercedess-MacBook-Pro-2.local>
2023-02-23 20:29:21 -0800

    exploratory-analysis-alexis-dimas

--- Table.R
+++ Table.R
@@ -3,14 +3,6 @@ library("dplyr")
 # Load Tinder Data
 tinder_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-alexis-dimas/main/Tinder%20Millennial%20Match%20Rate%20Dataset.csv", stringsAsFactors = FALSE)
 
-tinder_df <- read.csv("Tinder millennial Match Rate dataset.csv", stringsAsFactors = FALSE)
+#tinder_df <- read.csv("Tinder millennial Match Rate dataset.csv", stringsAsFactors = FALSE)
 
-
-gender <- trimws(gender)
-length(gender[gender == 'male respondents'])
-
-length(gender[gender == 'female respondents'])
-
-avg_percentage <- tinder_df %>% 
-  group_by(Percentage) %>%  
-  summarize(avg_percentage = mean(PercentageofRelationships))
+gender_df <- tinder_data %>% filter(Segment.Type == "Gender")
