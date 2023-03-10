545dc1adf80a9390fdac23eba47723692a2d9e75
alexis-dimas <alexisdimas81@gmail.com>
2023-03-10 10:42:13 -0800

    Complete Table

--- Table.R
+++ Table.R
@@ -4,6 +4,11 @@ library("kableExtra")
 # Load Tinder Data
 tinder_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-alexis-dimas/main/Tinder%20Millennial%20Match%20Rate%20Dataset.csv", stringsAsFactors = FALSE)
 
-gender_df <- tinder_data %>% filter(Segment.Type == "Gender")
+gender_df <- tinder_data %>% 
+  group_by(Segment.Type) %>% 
+  filter(Segment.Type == "Gender")
 
-gender_df %>% kbl() %>% kable_styling() %>% kable_paper("hover", full_width = F)
+gender_df %>% 
+  kbl() %>% 
+  kable_styling() %>% 
+  kable_paper("hover", full_width = F)
