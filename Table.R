cd0853f7523ca0de851e35a09672f00c4228de7f
alexis-dimas <alexisdimas81@gmail.com>
2023-03-10 10:35:57 -0800

    Fix issues and create a working webpage

--- Table.R
+++ Table.R
@@ -1,8 +1,9 @@
 library("dplyr")
+library("kableExtra")
 
 # Load Tinder Data
 tinder_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-alexis-dimas/main/Tinder%20Millennial%20Match%20Rate%20Dataset.csv", stringsAsFactors = FALSE)
 
-#tinder_df <- read.csv("Tinder millennial Match Rate dataset.csv", stringsAsFactors = FALSE)
-
 gender_df <- tinder_data %>% filter(Segment.Type == "Gender")
+
+gender_df %>% kbl() %>% kable_styling() %>% kable_paper("hover", full_width = F)
--- summary.R
+++ summary.R
@@ -6,36 +6,39 @@ tinder_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/explor
 #tinder_df <- read.csv("Tinder millennial Match Rate dataset.csv", stringsAsFactors = FALSE)
 
 
-#People from Moblie
+#People from Mobile
 
-people_moblie <- tinder_data %>%
-  filter(Segment.Type == "Mobile") %>%
-  select(Segment.Description, Count) 
+people_mobile <- tinder_data %>% 
+  filter(Segment.Type == "Mobile") %>% 
+  summarize(Count = sum(Count)) %>% 
+  pull(Count)
 
-#People fro web
+#People from web
   
-  people_web <- tinder_data %>%
-    filter(Segment.Type == "Web") %>%
-    select(Segment.Description, Count) 
+people_web <- tinder_data %>% 
+  filter(Segment.Type == "Web") %>% 
+  summarize(Count = sum(Count)) %>% 
+  pull(Count)
 
 #People from University 
   
-  people_University <- tinder_data %>%
-    filter(Segment.Type == "University") %>%
-    select(Segment.Description, Count)
+people_University <- tinder_data %>% 
+  filter(Segment.Type == "University") %>% 
+  summarize(Count = sum(Count)) %>% 
+  pull(Count)
 
 #Number of females 
   
-  female_respondents <- tinder_data %>%
-    filter(agrepl("Female Respondents", Segment.Description)) %>%
-    select(Segment.Description, Count)
+female_respondents <- tinder_data %>% 
+  filter(agrepl("Female Respondents", Segment.Description)) %>% 
+  summarize(Count = sum(Count)) %>% 
+  pull(Count)
 
 #Number of Male 
   
-  male_respondents <- tinder_data %>%
-    filter(agrepl("Male Respondents", Segment.Description)) %>%
-    select(Segment.Description, Count)
+male_respondents <- tinder_data %>% 
+  filter(agrepl("Male Respondents", Segment.Description)) %>% 
+  summarize(Count = sum(Count)) %>% 
+  pull(Count)
   
-  
-  all.equal(gender_df$Segment.Description, "Female Respondents")
   
